2a4c4d0ba14ef6a51a1bc0a48408232b4448ce36
ty <hengty174@gmail.com>
2022-01-05 11:05:49 +0700

    chage

--- src/components/authentication/Signup.js
+++ src/components/authentication/Signup.js
@@ -10,11 +10,19 @@ const Signup=()=>{
                         <li  className="nav-main__item">
                             <a href="#">Sign up</a>
                         </li>
+                       
                         <li className="nav-main__item">
                             <a href="#">Sign in</a>
                         </li>
                     </ul>
                 </nav>
+           </div>
+           <div className="div-body__section">
+               <input className="first-name__input" type="text" placeholder='first name'></input>
+               <input className="last-name__input" type="text" placeholder='last name'></input>
+               <input type="text" placeholder='email'></input>
+               <input type="password" placeholder='password'></input>
+               <input type="password" placeholder='confirmpassword'></input>
            </div>
         </div>
     )
