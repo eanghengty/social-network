a3ac0a93742ee0d476167f39b806e9453753876c
ty <hengty174@gmail.com>
2022-01-06 21:30:38 +0700

    start

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './App.css';
-import Homepage from './components/Homepage';
+
 
 function App() {
   return (
-    <Homepage></Homepage>
+    <div></div>
       
   );
 }
