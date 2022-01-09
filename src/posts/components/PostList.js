67c04121ad35cb3ce0182ce6d57896449c34834a
ty <hengty174@gmail.com>
2022-01-09 15:16:28 +0700

    only owner can see their own post

--- src/posts/components/PostList.js
+++ src/posts/components/PostList.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './PostList.css'
 import PostItem from './PostItem'
+
 const PostList=(props)=>{
     if(props.items.length===0){
         return(
@@ -18,10 +19,10 @@ const PostList=(props)=>{
             {
                 props.items.map((post)=>{
                    return(
-                       <div>
+                    
                             <PostItem key={post.id} id={post.id} title={post.title} image={post.image} description={post.description} address={post.address} creatorId={post.creator} coordinate={post.location}></PostItem>
-                            <PostItem key={post.id} id={post.id} title={post.title} image={post.image} description={post.description} address={post.address} creatorId={post.creator} coordinate={post.location}></PostItem>
-                       </div>
+                            
+                    
                     
                    ) 
                 })
--- src/posts/pages/UserPost.js
+++ src/posts/pages/UserPost.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import PostList from '../components/PostList.js'
+import {useParams} from 'react-router-dom'
  const post=[
-    {id:'1', title:'wat phnom' , image:'https://lh5.googleusercontent.com/p/AF1QipOPZ9mZ5NrhL55URJ7cdPixNEIEQpp5WF5jjE9v=w408-h306-k-no',description:'chilling, at wat phnom', address:'ផ្លូវវត្តភ្នំ, Phnom Penh' , creatorId:'1' , coordinate:{lat:'11.5760393',lng:'104.9230512'}}
+    {id:'1', title:'wat phnom' , image:'https://lh5.googleusercontent.com/p/AF1QipOPZ9mZ5NrhL55URJ7cdPixNEIEQpp5WF5jjE9v=w408-h306-k-no',description:'chilling, at wat phnom', address:'ផ្លូវវត្តភ្នំ, Phnom Penh' , creatorId:'1' , coordinate:{lat:'11.5760393',lng:'104.9230512'}},
+    {id:'2', title:'wat phnom' , image:'https://lh5.googleusercontent.com/p/AF1QipOPZ9mZ5NrhL55URJ7cdPixNEIEQpp5WF5jjE9v=w408-h306-k-no',description:'chilling, at wat phnom', address:'ផ្លូវវត្តភ្នំ, Phnom Penh' , creatorId:'2' , coordinate:{lat:'11.5760393',lng:'104.9230512'}}
  ]
  //
 const UserPost=()=>{
+    const userId = useParams().userId
+    const loadPost= post.filter(post=>post.creatorId === userId)
     return(
-        <PostList items={post}></PostList>
+        <PostList items={loadPost}></PostList>
     )
 }
 export default UserPost
