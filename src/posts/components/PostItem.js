bd449e57a1bf10da0662de70a597140cca36cc60
ty <hengty174@gmail.com>
2022-01-10 15:32:16 +0700

    map rendering

--- src/posts/components/PostItem.js
+++ src/posts/components/PostItem.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './PostItem.css'
+import Map from './Map'
+import './Modal.css'
 const PostItem=(props)=>{
     return(
         <li className="li-custom text-center list-group-item p-2 mb-3">
@@ -11,10 +13,30 @@ const PostItem=(props)=>{
                 <h3 className="custom-h3">{props.address}</h3>
                 <p>{props.description}</p>
                 </div>   
-            <div>
-                <button>view on map</button>
-                <button>edit</button>
-                <button>delete</button>
+            <div className="col">
+                <button  className="col-md-12 col-lg-3 col-12 btn-custom__1 btn btn-success" data-toggle="modal" data-target="#viewonmap">view on map</button>
+                {/* modal component from modal class */}
+                {/* <Modal coordinate={props.coordinate} address={props.address} description={props.description}></Modal> */}
+                <div className="modal" id="viewonmap">
+            
+            <div className="container modal-dialog">
+                <div className="modal-content">
+                <div className="modal-header">
+                    <div className="modal-title">{props.address}</div>
+                </div>
+                <div className="modal-body">
+                    <Map center={props.coordinate} zoom={16}></Map>
+                    {/* <h2>{props.coordinate}</h2> */}
+                    {/* console.log({props.coordinate}) */}
+                </div>
+                <div className="modal-footer">
+                    <button className="btn btn-success" data-dismiss="modal">close</button>
+                </div>
+            </div>
+            </div>
+        </div>
+                <button to={`/${props.id}`} className="col-md-12 col-lg-3 col-12 btn-custom__1 btn btn-success">edit</button>
+                <button className="col-md-12 col-lg-3 col-12 btn-custom__1 btn btn-success">delete</button>
                 </div> 
         </li>
     )
--- src/posts/pages/UserPost.js
+++ src/posts/pages/UserPost.js
@@ -2,8 +2,8 @@ import React from 'react'
 import PostList from '../components/PostList.js'
 import {useParams} from 'react-router-dom'
  const post=[
-    {id:'1', title:'wat phnom' , image:'https://lh5.googleusercontent.com/p/AF1QipOPZ9mZ5NrhL55URJ7cdPixNEIEQpp5WF5jjE9v=w408-h306-k-no',description:'chilling, at wat phnom', address:'ផ្លូវវត្តភ្នំ, Phnom Penh' , creatorId:'1' , coordinate:{lat:'11.5760393',lng:'104.9230512'}},
-    {id:'2', title:'wat phnom' , image:'https://lh5.googleusercontent.com/p/AF1QipOPZ9mZ5NrhL55URJ7cdPixNEIEQpp5WF5jjE9v=w408-h306-k-no',description:'chilling, at wat phnom', address:'ផ្លូវវត្តភ្នំ, Phnom Penh' , creatorId:'2' , coordinate:{lat:'11.5760393',lng:'104.9230512'}}
+    {id:'1', title:'wat phnom' , image:'https://lh5.googleusercontent.com/p/AF1QipOPZ9mZ5NrhL55URJ7cdPixNEIEQpp5WF5jjE9v=w408-h306-k-no',description:'chilling, at wat phnom', address:'ផ្លូវវត្តភ្នំ, Phnom Penh' , creatorId:'1' , location:{lat:11.5760393,lng:104.9230512}},
+    {id:'2', title:'wat phnom' , image:'https://lh5.googleusercontent.com/p/AF1QipOPZ9mZ5NrhL55URJ7cdPixNEIEQpp5WF5jjE9v=w408-h306-k-no',description:'chilling, at wat phnom', address:'ផ្លូវវត្តភ្នំ, Phnom Penh' , creatorId:'2' , location:{lat:11.5760393,lng:104.9230512}}
  ]
  //
 const UserPost=()=>{
